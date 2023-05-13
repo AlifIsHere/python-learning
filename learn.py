12a0c94bfa863245bf9c3557cdfa8f213f6c753b
AlifIsHere <alif.is1998@gmail.com>
2023-05-13 17:45:21 +0700

    let's start

---
+++ learn.py
@@ -0,0 +1,47 @@
+# print("Hello World!!!");
+
+# class Car:
+#     def __init__(self, brand, color):
+#         self.brand = brand
+#         self.color = color
+#         self.speed = 0
+
+#     def accelerate(self, acceleration):
+#         self.speed += acceleration
+
+#     def brake(self, deceleration):
+#         self.speed -= deceleration
+
+#     def display_info(self):
+#         print(f"Brand: {self.brand}, Color: {self.color}, Speed: {self.speed} km/h")
+
+
+# # Create car objects
+# car1 = Car("Toyota", "Red")
+# car2 = Car("BMW", "Blue")
+
+# # Display initial information
+# car1.display_info()
+# car2.display_info()
+
+# # Accelerate and brake cars
+# car1.accelerate(30)
+# car2.accelerate(50)
+
+# car1.brake(10)
+# car2.brake(20)
+
+# # Display updated information
+# car1.display_info()
+# car2.display_info()
+
+# name = input("Enter your name: ")
+# print("Hello, " + name + "!")
+
+# age = input("Enter your age: ")
+# print("You are " + age + " years old.")
+
+number = int(input("Enter your Number: "));
+
+for i in range(0, number):
+    print(i);
